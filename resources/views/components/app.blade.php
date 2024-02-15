f5ad59f08bee101dc3bb967f8b224d439b469743
Rafael Belchior <rafaelb89.rb@gmail.com>
2024-02-15 19:41:30 -0300

    update menu sistema

--- resources/views/components/app.blade.php
+++ resources/views/components/app.blade.php
@@ -12,24 +12,29 @@
 
     </head>
     <body class="bg-gray-200 text-black font-light">
-      <div class="flex justify-between container px-5 py-10 mx-auto">
-        <ul class=" cursor-pointer text-sm">
-          <li class="p-2 border border-gray-300 {{ request()->is('products*') ? 'bg-gray-300' : '' }}">
-            <a href="{{ route('products.index') }}">Produtos</a>
-          </li>
-          <li class="p-2 border border-gray-300 {{ request()->is('sales*') ? 'bg-gray-300' : '' }}">
-            <a href="{{ route('sales.index') }}">Vendas</a>
-          </li>
-          <li class="p-2 border border-gray-300 {{ request()->is('clients*') ? 'bg-gray-300' : '' }}">
-            <a href="{{ route('clients.index') }}">Clientes</a>
-          </li>
-        </ul>
-        <div class="flex flex-row-reverse p-10 items-center">
+      <div class="flex justify-between container px-5 py-10 mx-auto border border-gray-300 mt-4 rounded bg-gray-100">
+        <div>
+          <ul class="flex cursor-pointer text-sm">
+            <li class="p-2 border border-gray-300 {{ request()->is('products*') ? 'bg-gray-300' : '' }}">
+              <a href="{{ route('products.index') }}">Produtos</a>
+            </li>
+            <li class="p-2 border border-gray-300 {{ request()->is('sales*') ? 'bg-gray-300' : '' }}">
+              <a href="{{ route('sales.index') }}">Vendas</a>
+            </li>
+            <li class="p-2 border border-gray-300 {{ request()->is('clients*') ? 'bg-gray-300' : '' }}">
+              <a href="{{ route('clients.index') }}">Clientes</a>
+            </li>
+          </ul>
+        </div>
+        <div class="flex flex-row-reverse items-center">
           <form action="{{ route('logout') }}" method="POST">
             @csrf
-            <button type="submit" class="px-2 bg-gray-400 border rounded text-white ml-2">Sair</button>
+            <button type="submit" class="px-2 bg-gray-400 border rounded text-white ml-4">Sair</button>
           </form>
           <a type="text-xs">Bem vindo (a), {{auth()->user()->name}}</a>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 mr-1">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
+          </svg>
         </div>
       </div>
       <div class="container px-5 py-10 mx-auto">
